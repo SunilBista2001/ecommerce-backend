513ac1c405aa1379bea6d2d2f031f9c91589975e
Sunil <btwitsbista@gmail.com>
2023-06-06 08:37:29 +0545

    ignored the profile-routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@
 import { Router } from 'express'
 import userRouter from '../routes/user.routes'
 import authRouter from '../routes/auth.routes'
-import profileRouter from '../routes/profile.routes'
+// import profileRouter from '../routes/profile.routes'
 
 const router = Router()
 
--- src/routes/profile.routes.ts
+++ src/routes/profile.routes.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express'
 import { authenticateToken } from '../middlewares/authentication.middleware'
-import { getProfile, updateProfile } from '../controllers/profile.controller'
+// import { getProfile, updateProfile } from '../controllers/profile.controller'
 
 const router = Router()
 
-router.get('/', authenticateToken, getProfile)
+// router.get('/', authenticateToken, getProfile)
 
-router.put('/:id', updateProfile)
+// router.put('/:id', updateProfile)
 
 export default router
